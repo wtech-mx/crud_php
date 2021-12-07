6057deceb2da0e747e4b066c99c89984aba16382
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2021-12-07 08:54:20 -0600

    actualizacion de cambistittles

--- facturacion/modal_add.php
+++ facturacion/modal_add.php
@@ -3,28 +3,38 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Modal Title</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h4 class="modal-title">Datos del Cliente</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <p>The content of your modal.2</p>
+                    <p></p>
                     <form>
                         <div class="row">
                             <div class="col-sm-6">
-                                <div class="form-group mb-3"><label class="form-label">Label</label><input class="form-control" type="text"></div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">R.F.C</label>
+                                <input class="form-control" type="text"></div>
                             </div>
                             <div class="col-sm-6">
-                                <div class="form-group mb-3"><label class="form-label">Label</label><input class="form-control" type="text"></div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">Nombre o razo social</label>
+                                <input class="form-control" type="text"></div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-sm-4">
-                                <div class="form-group mb-3"><label class="form-label">Label</label><input class="form-control" type="text"></div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">CURP</label>
+                                <input class="form-control" type="text"></div>
                             </div>
                             <div class="col-sm-4">
-                                <div class="form-group mb-3"><label class="form-label">Label</label><input class="form-control" type="text"></div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">Tx id</label>
+                                <input class="form-control" type="text"></div>
                             </div>
                             <div class="col-sm-4">
-                                <div class="form-group mb-3"><label class="form-label">Label</label><input class="form-control" type="text"></div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">No Licencia</label>
+                                <input class="form-control" type="text"></div>
                             </div>
                         </div>
                     </form>
