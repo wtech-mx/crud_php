16d101695952eca9dccf36df28ca202d97fcb169
verplancken <42614785+verplancken@users.noreply.github.com>
2021-12-07 00:04:23 -0600

    facturacion

---
+++ facturacion/ajax/listar_productos.php
@@ -0,0 +1,104 @@
+<?php
+/* Connect To Database*/
+	require_once ("../../conexion.php");
+ 
+	
+$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
+if($action == 'ajax'){
+	$query = mysqli_real_escape_string($con,(strip_tags($_REQUEST['query'], ENT_QUOTES)));
+ 
+	$tables="cporte_tvehiculos";
+	$campos="*";
+	$sWhere=" cporte_tvehiculos.placa LIKE '%".$query."%'";
+	$sWhere.=" order by cporte_tvehiculos.placa";
+	
+	
+	include 'pagination.php'; //include pagination file
+	//pagination variables
+	$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
+	$per_page = intval($_REQUEST['per_page']); //how much records you want to show
+	$adjacents  = 4; //gap between pages after number of adjacents
+	$offset = ($page - 1) * $per_page;
+	//Count the total number of row in your table*/
+	$count_query   = mysqli_query($con,"SELECT count(*) AS numrows FROM $tables where $sWhere ");
+	if ($row= mysqli_fetch_array($count_query)){$numrows = $row['numrows'];}
+	else {echo mysqli_error($con);}
+	$total_pages = ceil($numrows/$per_page);
+	//main query to fetch the data
+	$query = mysqli_query($con,"SELECT $campos FROM  $tables where $sWhere LIMIT $offset,$per_page");
+	//loop through fetched data
+	
+ 
+ 
+		
+	
+	if ($numrows>0){
+		
+	?>
+		<div class="table-responsive">
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th class='text-center'>Placa</th>
+						<th>Año </th>
+						<th>Tipo </th>
+						<th class='text-center'>Tipo Remolque 1</th>
+						<th class='text-right'>Placa 1</th>
+
+						<th class='text-center'>Tipo Remolque 2</th>
+						<th>Placa 2</th>
+						<th>Permiso SCT</th>
+						<th class='text-center'># Permiso</th>
+						<th class='text-right'>Aseguradora resp. civil</th>
+						<th class='text-right'>Aseguradora de la carga</th>
+						<th class='text-right'># Póliza de la carga</th>
+						<th class='text-right'>Aseguradora del medio ambiente</th>
+						<th class='text-right'># Póliza medio ambiente</th>
+						<th class='text-right'>Prima del seguro</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>	
+						<?php 
+						$finales=0;
+						while($row = mysqli_fetch_array($query)){	
+							$product_id=$row['id'];
+							$prod_code=$row['prod_code'];
+							$placa=$row['placa'];
+							$prod_ctry=$row['prod_ctry'];
+							$prod_qty=$row['prod_qty'];
+							$price=$row['price'];						
+							$finales++;
+						?>	
+						<tr class="<?php echo $text_class;?>">
+							<td class='text-center'><?php echo $prod_code;?></td>
+							<td ><?php echo $placa;?></td>
+							<td ><?php echo $prod_ctry;?></td>
+							<td class='text-center' ><?php echo $prod_qty;?></td>
+							<td class='text-right'><?php echo number_format($price,2);?></td>
+							<td>
+								<a href="#"  data-target="#editProductModal" class="edit" data-toggle="modal" data-code='<?php echo $prod_code;?>' data-name="<?php echo $placa?>" data-category="<?php echo $prod_ctry?>" data-stock="<?php echo $prod_qty?>" data-price="<?php echo $price;?>" data-id="<?php echo $product_id; ?>"><i class="material-icons" data-toggle="tooltip" title="Editar" >&#xE254;</i></a>
+								<a href="#deleteProductModal" class="delete" data-toggle="modal" data-id="<?php echo $product_id;?>"><i class="material-icons" data-toggle="tooltip" title="Eliminar">&#xE872;</i></a>
+                    		</td>
+						</tr>
+						<?php }?>
+						<tr>
+							<td colspan='6'> 
+								<?php 
+									$inicios=$offset+1;
+									$finales+=$inicios -1;
+									echo "Mostrando $inicios al $finales de $numrows registros";
+									echo paginate($page, $total_pages, $adjacents);
+								?>
+							</td>
+						</tr>
+				</tbody>			
+			</table>
+		</div>	
+ 
+	
+	
+	<?php	
+	}	
+}
+?>          
---
+++ facturacion/index.php
@@ -0,0 +1,83 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>CRUD de Facturación </title>
+
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cfdi.facturoporti.com.mx/css/bootstrap.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/custom.css"> -->
+
+
+	<script	script src="https://cfdi.facturoporti.com.mx/Scripts/modernizr-2.8.3.js" type="text/javascript"></script>
+    <link href="https://cfdi.facturoporti.com.mx/css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="https://cfdi.facturoporti.com.mx/css/BootstrapTable/116/bootstrap-table.css" rel="stylesheet" type="text/css" />
+
+    <link href="https://cfdi.facturoporti.com.mx/css/sticky-footer-navbar.css" rel="stylesheet" type="text/css" />
+    <link href="https://cfdi.facturoporti.com.mx/css/theme.css" rel="stylesheet" type="text/css" />
+
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/jquery.validate.min.js" type="text/javascript"></script>
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/jquery.validate.unobtrusive.min.js" type="text/javascript"></script>
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/jquery.form.min.js" type="text/javascript"></script>
+    
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/SesionMasterBootStrap" type="text/javascript"></script>
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/jquery-ui-1.12.1.min.js" type="text/javascript"></script>
+        
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/SesionMaster-2" type="text/javascript"></script>
+    
+    <script src="https://cfdi.facturoporti.com.mx/Scripts/FontAwesome/all.min.js" type="text/javascript"></script>
+    
+
+
+</head>
+<body>
+ 
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+						<h2>Administrar </b></h2>
+					</div>
+					<div class="col-sm-6">
+						<a href="#FormAutoTransporte" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Agregar nuevo producto</span></a>
+					</div>
+                </div>
+            </div>
+			<div class='col-sm-4 pull-right'>
+				<div id="custom-search-input">
+                            <div class="input-group col-md-12">
+                                <input type="text" class="form-control" placeholder="Buscar"  id="q" onkeyup="load(1);" />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-info" type="button" onclick="load(1);">
+                                        <span class="glyphicon glyphicon-search"></span>
+                                    </button>
+                                </span>
+                            </div>
+                </div>
+			</div>
+			<div class='clearfix'></div>
+			<hr>
+			<div id="loader"></div><!-- Carga de datos ajax aqui -->
+			<div id="resultados"></div><!-- Carga de datos ajax aqui -->
+			<div class='outer_div'></div><!-- Carga de datos ajax aqui -->
+            
+			
+        </div>
+    </div>
+	<!-- Edit Modal HTML -->
+	<?php include("modal_add.php");?>
+	<!-- Edit Modal HTML -->
+	<?php include("modal_edit.php");?>
+	<!-- Delete Modal HTML -->
+	<?php include("modal_delete.php");?>
+	<script src="js/script.js"></script>
+</body>
+</html>
